8f934ce2a2993ea4733b1b726ca590d351b3a0d8
Mark Rotteveel <mark@lawinegevaar.nl>
2023-07-02 11:29:26 +0200

    #16 Replace automatic module name with module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module nl.lawinegevaar.exttablegen {
+    requires com.opencsv;
+    requires info.picocli;
+    requires org.apache.commons.lang3;
+    requires jakarta.xml.bind;
+    requires java.logging;
+}
